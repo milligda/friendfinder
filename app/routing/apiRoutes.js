fd58818f275a90fec4c6e4d221eb0ecc64c52caf
DMilligan <dmilligan@springlocker.com>
2018-07-26 12:01:25 -0400

    adding user validation testing to the survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -27,28 +27,39 @@ module.exports = function(app) {
     // receive the user submission
     app.post('/api/activities', function (req, res) {
 
+        // add the user submission to the user data file
+        userData.push(req.body);
+
+        // store the user's choices as a variable
         var userScores = req.body.scores;
 
+        // clear the output variables before comparing the user's choices
         var activitySuggestion = '';
         var currentLowScore = 100;
 
-        // for each activity
+        // for each activity compare the user's choices to the activities scores
         for (var i = 0; i < activitiesData.length; i++) {
 
+            // set the matchScore to 0 at the start of the score comparison
             var matchScore = 0;
 
-            // for each score within the activity
+            // for each score within the activity compare the user's choice versus the activity's score for that question
             for (var j = 0; j < activitiesData[i].scores.length; j++) {
 
                 // convert the user choice to an integer
                 var userChoice = parseInt(userScores[j]);
+
+                // store the activity's score for that question
                 var activityScore = activitiesData[i].scores[j];
 
                 // add the variance between the user's choice and the activity score to the matchScore
                 matchScore += Math.abs(activityScore - userChoice);
             }
 
+            // after comparing all the user's choices to the activity's scores, see if it is a better match than the current low score
+            // if it is a better match, set the new currentLowScore and set the activitySuggestion name
             if (matchScore < currentLowScore) {
+
                 currentLowScore = matchScore;
                 activitySuggestion = activitiesData[i].name;
 
@@ -63,6 +74,7 @@ module.exports = function(app) {
         // store the user response in the 
         // userData.push(req.body);
 
+        // return the activitySuggestion in a way that it can be added to a url and displayed
         return res.json(userData);
         
     });
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,61 +1,105 @@
-$(document).ready(function() {
 
-    function populateQuestions() {
+// ==============================================================================
+// function for displaying survey questions
+// ==============================================================================
 
-        for (var i = 0; i < questions.length; i++) {
-            var question = $('<div class="form-group">');
-            var id = 'question' + (i + 1);
-            var label = $('<label for="' + id + '" class="question-header">' + questions[i].title + '</label>');
-            var select = $('<select class="form-control select-box" id="' + id + '">');
-            var defaultOption = $('<option selected>Select an option</option>');
-            defaultOption.appendTo(select);
-            label.appendTo(question);
+function populateQuestions() {
 
-            for (var j = 0; j < questions[i].option.length; j++) {
-                var option = $('<option value="' + (j + 1) + '">' + questions[i].option[j] + '</option>');
-                option.appendTo(select);
-            }
-            select.appendTo(question);
-            question.appendTo('#survey-form');
+    // cycle through each question
+    for (var i = 0; i < questions.length; i++) {
+
+        // create the div container for the question, the id, the label element, the select element and the default select option
+        var question = $('<div class="form-group">');
+        var id = 'question' + (i + 1);
+        var label = $('<label for="' + id + '" class="question-header">' + questions[i].title + '</label>');
+        var select = $('<select class="form-control select-box " id="' + id + '">');
+        var defaultOption = $('<option selected>Select an option</option>');
+
+        // append the pieces to the question div
+        defaultOption.appendTo(select);
+        label.appendTo(question);
+
+        // cycle through each option and append them to the select element
+        for (var j = 0; j < questions[i].option.length; j++) {
+            var option = $('<option value="' + (j + 1) + '">' + questions[i].option[j] + '</option>');
+            option.appendTo(select);
         }
 
-        var submitButton = $('<button type="submit" id="submit-btn" class="btn btn-primary">Submit</button>');
-        submitButton.appendTo('#survey-form');
+        // append the select element to the question div and append the question div to the form
+        select.appendTo(question);
+        question.appendTo('#survey-form');
     }
 
-    populateQuestions();
-});
+    // after all the questions have been added, create the submit button and append it to the form
+    var submitButton = $('<button type="submit" id="submit-btn" class="btn btn-primary">Submit</button>');
+    submitButton.appendTo('#survey-form');
+}
+
+// ==============================================================================
+// Survey Event Listener
+// ==============================================================================
 
 $(document).on('click', '#submit-btn', function (event) {
     event.preventDefault();
 
     // create an array to store the user scores
     var userScores = [];
+    var userName = $('#name-input').val().trim();
 
-    // for each question, store the answer and push it to the userScores array
-    for (var i = 1; i < questions.length + 1; i++) {
+    var surveyComplete = true;
 
-        // store the answer
-        var score = $('#question' + i).val().trim();
+    // check if the user name was entered
+    if (userName == "") {
 
-        // push the answer to the array as an integer
-        userScores.push(parseInt(score));
+        alert("Please enter your name before submitting.");
+
+    } else {
+
+        // cycle through each question
+        for (var i = 1; i < questions.length + 1; i++) {
+
+            // store the answer
+            var score = $('#question' + i).val().trim();
+
+            // check to make sure that the answer is not NaN
+            if (isNaN(score)) {
+
+                // if it is, change surveyComplete to false
+                surveyComplete = false;
+
+                // alert the user to fill in all questions and break the for loop
+                alert("Please answer all the questions before submitting.");
+                break;
+            }
+
+            // push the answer to the array as an integer
+            userScores.push(parseInt(score));
+        }
+
+        // if the survey is complete, create the newUser object and post it
+        if (surveyComplete) {
+
+            // create the newUser object with the user's name and scores
+            var newUser = {
+                name: userName,
+                scores: userScores
+            }
+
+            // post the newUser object.  the data received will be the activity which we will then redirect the user to
+            $.post('/api/activities', newUser, function () {
+
+                // redirect to the correct activity page
+                console.log("this was logged");
+            });
+        }
     }
+});
 
-    // create the newUser object with the user's name and scores
-    var newUser = {
-        name: $('#name-input').val().trim(),
-        scores: userScores
-    }
+// ==============================================================================
+// When the survey page is ready, display the questions
+// ==============================================================================
 
-    // console.log(newUser);
+$(document).ready(function() {
 
-    // post the newUser object.  the data received will be the activity which we will then redirect the user to
-    $.post('/api/activities', newUser, function (data) {
-
-        // redirect to the correct activity page
-        console.log(data);
-
-    });
-
+    populateQuestions();
 });
